03031222e23081a437f1f2194b2ab1e82dbc3f9f
Diksha <diksha9421@gmail.com>
2024-02-20 23:53:32 +0000

    Add existing project files to Git

---
+++ src/main/java/com/movie/movierentals/MovieRentalsApplication.java
@@ -0,0 +1,13 @@
+package com.movie.movierentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie/movierentals/MovierentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.movierentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
